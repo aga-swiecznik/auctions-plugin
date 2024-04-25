2fed8e8d4d1875af12e474425998a02005478f09
Agnieszka Swiecznik <agnieszka.swiecznik@theguarantors.com>
2024-04-25 12:11:49 +0200

    no offers text

--- src/app/[groupId]/_components/NoOffersModal.tsx
+++ src/app/[groupId]/_components/NoOffersModal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { WorkOff, WorkHistory } from "@mui/icons-material";
 import { Box, IconButton } from "@mui/material";
@@ -12,52 +12,59 @@ interface Props {
   noOffersYet: boolean;
 }
 
-export const NoOffersModal = ({auctionId, noOffers, noOffersYet} : Props) => {
+export const NoOffersModal = ({ auctionId, noOffers, noOffersYet }: Props) => {
   const { setText } = useCopyDialog();
   const updateMutation = useAuctionMutation(() => {
     !noOffers && setText(modalText);
   });
 
   const toggleNoOffer = () => {
-    updateMutation.mutate({ auction: { id: auctionId, noOffers: true }});
-  }
+    updateMutation.mutate({ auction: { id: auctionId, noOffers: true } });
+  };
 
   // const toggleNoOfferYet = () => {
   //   updateMutation.mutate({ auction: { id: auctionId, noOffers: false, noOffersYet: true }});
   // }
 
   const toggleOff = () => {
-    updateMutation.mutate({ auction: { id: auctionId, noOffers: false }});
+    updateMutation.mutate({ auction: { id: auctionId, noOffers: false } });
+  };
+
+  const modalText = `Szkoda, że tym razem się nie udało 🥹 Proszę, nie rezygnuj z pomocy 🫶🏼 Może spróbujesz wystawić swoją ofertę w wątku Kup Teraz?
+  https://www.facebook.com/groups/325336195551284/permalink/926791632072401/
+  Może tym razem się uda. Nigdy się nie poddajemy‼️
+  Dziękujemy z całego serca ❤💙
+  Uwaga! Post będzie usunięty przez administrację 3 dni po zakończeniu aukcji.`;
+
+  if (!noOffers) {
+    return (
+      <>
+        <Box component="span" sx={{ display: { xs: "none", sm: "inline" } }}>
+          Bez ofert?:{" "}
+        </Box>
+        <Tooltip title="Kliknij aby oznaczyć ze nie ma ofert">
+          <IconButton
+            size="small"
+            onClick={toggleNoOffer}
+            sx={{ color: "#daca33" }}
+          >
+            <WorkHistory />
+          </IconButton>
+        </Tooltip>
+      </>
+    );
   }
 
-  const modalText = `Szkoda, że tym razem się nie udało 🥹 Proszę, nie rezygnuj z pomocy 🫶🏼
-  Możesz wystawić licytacje w grupie TeamLeonaDlaBruna, która również
-  wspiera Bruna w walce o terapię genową 💪
-  https://www.facebook.com/groups/licytacjedlaleonasma/
-  Może tym razem się uda. Nigdy się nie poddajemy‼️
-  Dziękujemy z całego serca ❤💙`
-
-  if(!noOffers) {
-    return <>
-      <Box component="span" sx={{ display: { xs: 'none', sm: 'inline' } }}>
-        Bez ofert?: {" "}
+  return (
+    <>
+      <Box component="span" sx={{ display: { xs: "none", sm: "inline" } }}>
+        Bez ofert:{" "}
       </Box>
-      <Tooltip title="Kliknij aby oznaczyć ze nie ma ofert">
-        <IconButton size="small" onClick={toggleNoOffer} sx={{ color: '#daca33' }}>
-          <WorkHistory />
+      <Tooltip title="Kliknij aby odznaczyć">
+        <IconButton size="small" onClick={toggleOff} sx={{ color: "#aa0000" }}>
+          <WorkOff />
         </IconButton>
       </Tooltip>
     </>
-  }
-
-  return <>
-    <Box component="span" sx={{ display: { xs: 'none', sm: 'inline' } }}>
-      Bez ofert: {" "}
-    </Box>
-    <Tooltip title="Kliknij aby odznaczyć">
-      <IconButton size="small" onClick={toggleOff} sx={{ color: '#aa0000' }}>
-        <WorkOff />
-      </IconButton>
-    </Tooltip>
-  </>
-}
+  );
+};
